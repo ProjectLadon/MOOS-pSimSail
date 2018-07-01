d6c99fca54dee554e52775e3676c1aef8f0a50c3
Pierce Nichols <pierce@logos-electro.com>
2018-06-30 18:07:09 -0700

    Initial commit

---
+++ SimSail_Info.h
@@ -0,0 +1,18 @@
+/****************************************************************/
+/*   NAME:                                              */
+/*   ORGN: MIT Cambridge MA                                     */
+/*   FILE: SimSail_Info.h                                      */
+/*   DATE: Dec 29th 1963                                        */
+/****************************************************************/
+
+#ifndef SimSail_INFO_HEADER
+#define SimSail_INFO_HEADER
+
+void showSynopsis();
+void showHelpAndExit();
+void showExampleConfigAndExit();
+void showInterfaceAndExit();
+void showReleaseInfoAndExit();
+
+#endif
+
